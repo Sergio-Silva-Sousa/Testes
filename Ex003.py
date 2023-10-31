2cefc194e142de0dd5587acd6be66231524a84a3
Sérgio <149341860+Sergio-Silva-Sousa@users.noreply.github.com>
2023-10-30 23:26:59 -0300

    Add files via upload

---
+++ Ex003.py
@@ -0,0 +1,4 @@
+n1 = int(input('\033[1mDigite um valor:\033[m'))
+n2 = int(input('\033[1mDigite outro valor:\033[m '))
+s = n1 + n2
+print(f"\033[1;32mA soma entre {n1} e {n2} é igual a {s}!\033[m")
